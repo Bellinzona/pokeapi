903778379479961e3f28b5e6c90cab7c6e7b93e9
bellinzona <mtbellinzona@gmail.com>
2023-10-31 17:34:58 -0300

    incluyendo historial

--- main.js
+++ main.js
@@ -4,12 +4,71 @@ let btn = document.querySelector("button")
 let incorrecto = document.querySelector(".incorrecto")
 let bodystyle = document.querySelector("body")
 let informacion = document.querySelector(".informacion")
+let pokemonesBuscados = document.querySelector(".pokemonesBuscados")
+let pokeList = []
+
+
+function pokemon(imagen, nombre, tipo, numero) {
+    this.imagen = imagen
+    this.nombre = nombre
+    this.tipo = tipo
+    this.numero = numero
+
+
+}
 
 
 
-let cargarPokemon = async function(poke){
-    try{
-        
+function pokemonGuardado(a, b, c, d) {
+    let pokemonG = document.createElement("div")
+    let imagen = document.createElement("img")
+    let nombre = document.createElement("p")
+    let tipo = document.createElement("p")
+    let numero = document.createElement("p")
+
+    pokemonG.classList.add("pokemonG")
+    nombre.classList.add("nombre")
+    tipo.classList.add("tipo")
+    numero.classList.add("numero")
+
+    imagen.src = a
+    nombre.textContent = b
+    tipo.textContent = c
+    numero.textContent = d
+
+    pokemonG.appendChild(imagen)
+    pokemonG.appendChild(nombre)
+    pokemonG.appendChild(tipo)
+    pokemonG.appendChild(numero)
+
+    return pokemonG
+
+
+
+}
+
+function guardarPokeListEnLocalStorage() {
+    localStorage.setItem("pokeList", JSON.stringify(pokeList));
+}
+
+
+function cargarPokeListDesdeLocalStorage() {
+    const pokeListGuardada = localStorage.getItem("pokeList");
+    if (pokeListGuardada) {
+        pokeList = JSON.parse(pokeListGuardada);
+        console.log(pokeList)
+        pokemonesBuscados.style.display = "flex"
+        for(let i = 0; i < pokeList.length; i++)
+            pokemonesBuscados.appendChild(pokemonGuardado(pokeList[i].imagen,pokeList[i].nombre,pokeList[i].tipo,pokeList[i].numero))
+            
+    }
+}
+
+cargarPokeListDesdeLocalStorage();
+
+let cargarPokemon = async function (poke) {
+    try {
+
         console.log()
 
         const rta = await fetch(`https://pokeapi.co/api/v2/pokemon/${input.value}`);
@@ -31,7 +90,25 @@ let cargarPokemon = async function(poke){
         container.style.justifyContent = "flex-start"
         container.style.alignItems = "flex-start"
 
-        switch(datos.types[0].type.name){
+
+
+
+        pokemonesBuscados.appendChild(pokemonGuardado(datos.sprites.front_default, datos.name, datos.types[0].type.name, datos.order))
+
+        let pokemonO = new pokemon(datos.sprites.front_default, datos.name, datos.types[0].type.name, datos.order)
+        pokeList.push(pokemonO)
+        guardarPokeListEnLocalStorage();
+        console.log(pokeList)
+
+
+
+        pokemonesBuscados.style.display = "flex"
+
+
+
+
+
+        switch (datos.types[0].type.name) {
 
             case "poison":
                 bodystyle.style.background = "#F912C1"
@@ -39,8 +116,8 @@ let cargarPokemon = async function(poke){
             case "water":
                 bodystyle.style.background = "#12C8F9"
                 break
-                
-                
+
+
 
             case "normal":
                 bodystyle.style.background = "#C18463"
@@ -59,7 +136,7 @@ let cargarPokemon = async function(poke){
             case "fighting":
                 bodystyle.style.background = "#F67733"
                 break
-                
+
 
             case "electric":
                 bodystyle.style.background = "rgb(193, 235, 8)"
@@ -85,40 +162,40 @@ let cargarPokemon = async function(poke){
             case "psychic":
                 bodystyle.style.background = "#de0dd0"
                 break
-            
-        
+
+
 
 
             default:
                 bodystyle.style.background = "white"
 
-            
 
-            
+
+
         }
-        
 
 
 
-    }catch(error){
+
+    } catch (error) {
         incorrecto.style.display = "flex"
         bodystyle.style.background = "black"
         setTimeout(() => {
-        incorrecto.style.display = "none"
-        
-        
-    }, 2500);
-        
+            incorrecto.style.display = "none"
+
+
+        }, 2500);
+
     }
-    
+
 
 }
 
 
 
-let tiposPokemon = async function(){
+let tiposPokemon = async function () {
 
-    try{
+    try {
 
         let poketipos = await fetch(`https:pokeapi.co/api/v2/type`)
         let datos = await poketipos.json()
@@ -129,11 +206,11 @@ let tiposPokemon = async function(){
 
 
 
-    }catch{
+    } catch {
         console.log()
     }
 
-    
+
 
 }
 
@@ -142,22 +219,22 @@ let tiposPokemon = async function(){
 
 
 
-btn.addEventListener("click", function(){
-    if (input.value == ""){
+btn.addEventListener("click", function () {
+    if (input.value == "") {
         incorrecto.style.display = "flex"
         setTimeout(() => {
             incorrecto.style.display = "none"
-            
-            
+
+
         }, 2500);
 
-    }else{
-            cargarPokemon(input)
-            informacion.style.display = "flex"
+    } else {
+        cargarPokemon(input)
+        informacion.style.display = "flex"
 
 
-       
-        
+
+
 
     }
 })
